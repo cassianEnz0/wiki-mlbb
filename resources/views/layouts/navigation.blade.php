bd462273a55418033eeef530f315e1ce87fb155b
kariniblu-1a <bintangkarini06@gmail.com>
2025-12-03 18:17:16 +0700

    upgrade excel

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -56,6 +56,11 @@
                             </x-slot>
                         </x-dropdown>
                     </div>
+
+                    {{-- LINK BARU: VIEW (EXPORT EXCEL) --}}
+                    <x-nav-link :href="route('heroes.export.excel')" :active="request()->routeIs('heroes.export.excel')">
+                        {{ __('View') }}
+                    </x-nav-link>
                 </div>
 
             </div>
@@ -126,6 +131,11 @@
         <div class="pt-2 pb-3 space-y-1">
             <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                 {{ __('Dashboard') }}
+            </x-responsive-nav-link>
+
+            {{-- LINK BARU DI MENU MOBILE --}}
+            <x-responsive-nav-link :href="route('heroes.export.excel')" :active="request()->routeIs('heroes.export.excel')">
+                {{ __('View') }}
             </x-responsive-nav-link>
         </div>
 
